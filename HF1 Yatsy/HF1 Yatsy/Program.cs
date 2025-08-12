161f4ab948c614dbb4c5d19a68fc085d8789d280
GhosTzRider <marcus@rollmann.dk>
2025-08-12 11:27:59 +0200

    Yatzy game opgave

--- HF1 Yatsy/HF1 Yatsy/Program.cs
+++ HF1 Yatsy/HF1 Yatsy/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace HF1_Yatsy
+﻿using System;
+using System.Collections.Generic;
+
+namespace HF1_Yatsy
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
         }
     }
 }
+
+public class Dice
+{
+    public int roll()
+    {
+        Random random = new Random();
+        return random.Next(1, 7);
+    }
+}
+
+public class Spiller
+{
+    public string Name { get; set; }
+    public int Score { get; set; }
+    public Spiller(string name)
+    {
+        Name = name;
+        Score = 0;
+    }
+    public void AddScore(int points)
+    {
+        Score += points;
+    }
+}
+
